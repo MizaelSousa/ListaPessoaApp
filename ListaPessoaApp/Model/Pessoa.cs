132484ccef117e20c66ae0acdf785cfae924cd5d
Mizael Sousa <aluno@MacBook-Pro-de-Aluno.local>
2019-03-19 21:53:15 -0400

    primeiro commit do procommit do projeto

---
+++ ListaPessoaApp/Model/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ListaPessoaApp.Model
+{
+    public class Pessoa
+    {
+        public string nome { set; get; }
+        public string sobrenome { set; get; }
+        public string dtCadastro { set; get; }
+
+        public string nomeCompleto
+        {
+            get { return nome + " " + sobrenome; }
+        }
+
+        public Pessoa()
+        {
+        }
+    }
+}
---
+++ ListaPessoaApp/PageListViewPessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace ListaPessoaApp
+{
+    public class PageListViewPessoa : ContentView
+    {
+        public PageListViewPessoa()
+        {
+            Content = new Label { Text = "Hello ContentView" };
+        }
+    }
+}
+
